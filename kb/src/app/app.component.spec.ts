e3c9d72dc87d99b9e6fa3faf1885817acbe60a52
Tobias Pickel <tobias.pickel@rsu-reifen.de>
2017-04-10 12:23:33 +0200

    ng cli update

--- kb/src/app/app.component.spec.ts
+++ kb/src/app/app.component.spec.ts
@@ -17,16 +17,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'hv works!'`, async(() => {
+  it(`should have as title 'kb works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hv works!');
+    expect(app.title).toEqual('kb works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('hv works!');
+    expect(compiled.querySelector('h1').textContent).toContain('kb works!');
   }));
 });
