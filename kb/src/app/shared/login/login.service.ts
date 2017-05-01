002acbc41c7618242bc3b0d8beb0ff9b74bb6a39
sanderdrummer <funkpunkdrummer@gmail.com>
2017-05-01 10:11:24 +0200

    implemented login

--- kb/src/app/shared/login/login.service.ts
+++ kb/src/app/shared/login/login.service.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AngularFire, FirebaseAuthState} from 'angularfire2';
 
 @Injectable()
 export class LoginService {
 
-  constructor(private af:AngularFire) {}
-
-  authByLocalStorage():firebase.Promise<FirebaseAuthState> {
-    const email = localStorage.getItem('email');
-    const password = localStorage.getItem('password');
-    return this.authByParams(email , password);
+  constructor(private af: AngularFire) {
   }
 
-  saveLogin(email:string, password:string):void {
+  authByLocalStorage(): firebase.Promise<FirebaseAuthState> {
+    const email = localStorage.getItem('email');
+    const password = localStorage.getItem('password');
+    return this.authByParams(email, password);
+  }
+
+  saveLogin(email: string, password: string): void {
     localStorage.setItem('email', email);
     localStorage.setItem('password', password);
   }
@@ -21,7 +22,7 @@ export class LoginService {
     localStorage.clear();
   }
 
-  authByParams(email:string, password:string):firebase.Promise<FirebaseAuthState>{
-    return this.af.auth.login({ email: email, password: password })
+  authByParams(email: string, password: string): firebase.Promise<FirebaseAuthState> {
+    return this.af.auth.login({email: email, password: password})
   }
 }
