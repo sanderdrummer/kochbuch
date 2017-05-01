4812f1503bf5815fefd9a25ae9cffe5fca298fe0
sanderdrummer <funkpunkdrummer@gmail.com>
2017-05-01 10:10:56 +0200

    implemented login

--- kb/src/app/settings/login/login.component.ts
+++ kb/src/app/settings/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {LoginService} from '../../shared/login/login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'kb-login',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private loginService: LoginService) {
+  }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  tryLogin(value: any): void {
+    const {email, password} = value;
+    this.loginService.authByParams(email, password).then(
+      res => this.onLoginSuccess(email, password),
+      err => this.onLoginError()
+    );
+  }
+
+  onLoginSuccess(email: string, password: string) {
+    this.loginService.saveLogin(email, password);
+    this.router.navigate(['../']);
+  }
+
+  onLoginError() {
+    this.loginForm.reset();
   }
 
 }
