17fcf9f985c94953ac84105ffe1239bc70ee0c8e
Tobias Pickel <tobias.pickel@rsu-reifen.de>
2017-05-02 08:29:26 +0200

    implemented select focus event and added interface

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,16 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, AfterViewInit, ViewChild} from '@angular/core';
 import {ProductStore} from '../shared/product.store';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, AbstractControl} from '@angular/forms';
 import {ProductModel} from '../shared/product.model';
 import {ProductService} from '../shared/product.service';
+import {ProductListComponentInterface} from './product-list-component.interface';
 
 @Component({
   selector: 'kb-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit, ProductListComponentInterface {
   @Output() onSelect = new EventEmitter();
   @ViewChild('amount') private amountElement: ElementRef;
   @ViewChild('filterName') private filterElement: ElementRef;
@@ -25,27 +26,27 @@ export class ProductListComponent implements OnInit {
 
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.filterForm = this.fb.group({
       filter: [''],
     });
     this.amountForm = this.fb.group({
       amount: [1]
     });
-
-    this.filterForm.controls.filter.valueChanges.subscribe(value => {
-      this.filterProducts(value);
-    });
-
+    this.subscribeToFilterChanges();
     this.products$ = this.productStore.getFilteredProducts();
   }
 
-  ngAfterViewInit() {
-    this.filterElement.nativeElement.focus();
+  ngAfterViewInit(): void {
+    this.focusProduct();
+  }
+
+  subscribeToFilterChanges() {
+    const filterControl: AbstractControl = this.filterForm.controls.filter;
+    filterControl.valueChanges.subscribe(value => this.filterProducts(value));
   }
 
   addProduct(title: string): void {
-    console.log(title);
     const product = new ProductModel({title});
     this.productService.createProduct(title)
       .then(() => {
@@ -55,19 +56,35 @@ export class ProductListComponent implements OnInit {
 
   selectProduct(product: ProductModel): void {
     this.selectedProduct = product;
-    setTimeout(() => {
-      this.amountElement.nativeElement.focus();
-    },100);
+    this.focusAmount();
   }
 
   filterProducts(query: string): void {
     this.productStore.setFilteredProducts(query);
   }
 
-  selectProductWithAmount(amount: number) {
+  selectProductWithAmount(amount: number): void {
     this.onSelect.emit({
       selectedProduct: this.selectedProduct,
       amount: amount
     });
+  }
+
+  reset(): void {
+    this.filterForm.reset({filter: ''});
+    this.amountForm.reset({amount: 1});
+    this.focusProduct();
+  }
+
+  focusAmount(): void {
+    setTimeout(() => {
+      this.amountElement.nativeElement.focus();
+    }, 100);
+  }
+
+  focusProduct(): void {
+    setTimeout(() => {
+      this.filterElement.nativeElement.focus();
+    }, 100);
   }
 }
