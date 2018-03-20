8506fd9af43d370bee24fa141cdb25046eeaf687
tobias.pickel <tobias.pickel@rsu-reifen.de>
2018-03-20 08:01:26 +0100

    FEATURE: added routes fro amount creation

--- src/app/common/components/products/components/product-link/component/layout.tsx
+++ src/app/common/components/products/components/product-link/component/layout.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { Route, withRouter } from 'react-router';
-import { ProductLinkCreator } from '../../..';
-import ProductLinkSelector from './productLinkSelection';
+import ProductLinkCreator from './product-link-creator';
+import ProductLinkSelector from './product-link-selector';
 
 // tslint:disable-next-line:no-any
 const Layout: React.SFC<any> = (props) => (
   <>
     <Route path={props.match.url} exact={true} component={ProductLinkSelector} />
-    <Route path={props.match.url + '/:product'} exact={true} component={ProductLinkCreator}/>
+    <Route path={props.match.url + ':product'} exact={true} component={ProductLinkCreator}/>
   </>
 );
 
