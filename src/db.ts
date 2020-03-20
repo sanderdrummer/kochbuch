2150ab9dae9b95f19181ba8f90842706910aebe9
Tobias Pickel <tobias.pickel@xing.com>
2020-03-20 14:59:14 +0100

    refactor(db): update schema for lists

--- src/db.ts
+++ src/db.ts
@@ -1,27 +1,27 @@
 import Dexie from "dexie";
 
 export interface ListItem {
-  id?: string;
   title: string;
   inBasket: boolean;
 }
 
 export interface Recipe {
-  id?: string;
   title: string;
-  tags: string[];
+  tags: string;
   ingredients: string[];
   description: string;
 }
+
+export const DEFAULT_PAGE_SIZE = 1;
 
 export class ListDb extends Dexie {
   listItems: Dexie.Table<ListItem, string>;
   recipes: Dexie.Table<Recipe, string>;
   constructor() {
-    super("ListDB2");
+    super("ListDB6");
     this.version(1).stores({
-      listItems: "++id,title,inBasket",
-      recipes: "++id,title"
+      listItems: "title",
+      recipes: "title,tags"
     });
     this.listItems = this.table("listItems");
     this.recipes = this.table("recipes");
@@ -30,10 +30,57 @@ export class ListDb extends Dexie {
 
 const db = new ListDb();
 
+export const getRecipeByName = async (title: string) => {
+  const [recipe] = await db.recipes
+    .where("title")
+    .equals(title)
+    .toArray();
+
+  return recipe;
+};
+
 export const getRecipes = () => {
   return db.recipes.toArray();
 };
 
+export const getPagedRecipes = (offset = 0) => {
+  return db.recipes
+    .offset(offset)
+    .limit(DEFAULT_PAGE_SIZE)
+    .toArray();
+};
+
+export const getRecipesCount = () => {
+  return db.recipes.count();
+};
 export const addRecipe = (recipe: Recipe) => {
   return db.recipes.add(recipe);
+};
+export const updateRecipe = (recipe: Recipe) => {
+  return db.recipes.put(recipe);
+};
+
+export const getList = () => {
+  return db.listItems.toArray();
+};
+
+export const addListItem = (listItem: ListItem) => {
+  return db.listItems.add(listItem);
+};
+
+export const addListItems = (listItems: string[]) => {
+  return db.listItems.bulkPut(
+    listItems.map(title => ({
+      title,
+      inBasket: false
+    }))
+  );
+};
+
+export const updateListItem = (listItem: ListItem) => {
+  return db.listItems.put(listItem);
+};
+
+export const clearList = () => {
+  return db.listItems.clear();
 };
