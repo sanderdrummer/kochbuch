ee4c64132b8cebb48859f7793981e57cbc35ff0c
Tobias Pickel <tobias.pickel@xing.com>
2020-05-06 16:29:34 +0200

    refactor: remove tesseract mocks

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,5 +7,3 @@ import "@testing-library/jest-dom/extend-expect";
 const Dexie = require("dexie");
 Dexie.dependencies.indexedDB = require("fake-indexeddb");
 Dexie.dependencies.IDBKeyRange = require("fake-indexeddb/lib/FDBKeyRange");
-
-jest.mock("tesseract.js");
