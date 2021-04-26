dfd06d794a2343702756bf7364fab2c9ad663b3b
sanderdrummer <tobias.pickel@moia.io>
2021-04-26 09:37:13 +0200

    fix: add react

--- src/plan/plan.tsx
+++ src/plan/plan.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Box, Button, Typography } from "@material-ui/core";
 import { useLocalStorage } from "../common/useLocalStorage";
 import { addListItems } from "../list/list-hooks";
