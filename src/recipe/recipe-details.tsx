f8ce48f60b79be507d7e486676cfac6922452012
sanderdrummer <tobias.pickel@moia.io>
2021-04-26 17:14:59 +0200

    feat: add amount modifier

--- src/recipe/recipe-details.tsx
+++ src/recipe/recipe-details.tsx
@@ -10,16 +10,25 @@ import {
   ListItem,
   ListItemText,
   Divider,
-  Grow,
+  TextField,
 } from "@material-ui/core";
 
 import { Recipe } from "./recipe-resource";
+
+const getAmount = (amount: string, modifier = 1) => {
+  if (modifier === 1) return amount;
+
+  const [digit = 0] = amount.match(/[0-9.]+/g) ?? [];
+  const [scale = ""] = amount.match(/[a-zA-Z]+/g) ?? [];
+  return `${Number(digit) * modifier}${scale}`;
+};
 
 export const RecipeDetails: React.FC<{
   recipe: Recipe;
   action?: React.ReactNode;
 }> = ({ recipe, action }) => {
   const [unfold, setUnfold] = React.useState(false);
+  const [modifier, setModifier] = React.useState(1);
 
   return (
     <>
@@ -32,36 +41,50 @@ export const RecipeDetails: React.FC<{
         <ListItemText primary={recipe.title} />
       </ListItem>
       <Divider />
-      <Grow in={unfold} mountOnEnter unmountOnExit>
+      {unfold && (
         <Box mt={3}>
           <Card>
             <CardHeader title={recipe?.title} action={action}></CardHeader>
           </Card>
+          <>
+            <Box mt={3}>
+              <Card>
+                <CardHeader
+                  subheader="Zutaten"
+                  action={
+                    <TextField
+                      type="number"
+                      value={modifier}
+                      inputProps={{
+                        step: 0.25,
+                      }}
+                      onChange={(e) => setModifier(Number(e.target.value))}
+                    />
+                  }
+                ></CardHeader>
 
-          <Box mt={3}>
-            <Card>
-              <CardHeader subheader="Zutaten"></CardHeader>
-              <List>
-                {recipe?.ingredients.map((ingredient) => (
-                  <ListItem key={ingredient.name}>
-                    {ingredient.amount} {ingredient.name}
-                  </ListItem>
-                ))}
-              </List>
-            </Card>
-          </Box>
-          <Box mt={3}>
-            <Card>
-              <CardHeader subheader="Zubereitung"></CardHeader>
-              <CardContent>
-                <Typography style={{ whiteSpace: "pre-wrap" }}>
-                  {recipe?.description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Box>
+                <List>
+                  {recipe?.ingredients.map((ingredient) => (
+                    <ListItem key={ingredient.name}>
+                      {getAmount(ingredient.amount, modifier)} {ingredient.name}
+                    </ListItem>
+                  ))}
+                </List>
+              </Card>
+            </Box>
+            <Box mt={3}>
+              <Card>
+                <CardHeader subheader="Zubereitung"></CardHeader>
+                <CardContent>
+                  <Typography style={{ whiteSpace: "pre-wrap" }}>
+                    {recipe?.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>
+          </>
         </Box>
-      </Grow>
+      )}
     </>
   );
 };
