0e6f6c796a7bab67a375f73b2014ade67c70fd12
sanderdrummer <tobias.pickel@moia.io>
2021-05-03 17:36:46 +0200

    fix: home route

--- src/routes-config.ts
+++ src/routes-config.ts
@@ -1,4 +1,4 @@
 export const BASE_PATH = "";
-export const RECIPES_PATH = BASE_PATH + "/rezepte";
+export const RECIPES_PATH = BASE_PATH + "/";
 export const LIST_PATH = BASE_PATH + "/einkaufliste";
 export const PLAN_PATH = BASE_PATH + "/plan";
