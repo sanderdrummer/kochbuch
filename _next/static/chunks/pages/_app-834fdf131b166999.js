6f2e91aa432e6bc4bbca7ee42dd12eb89e51c8e1
tobiaspickel <tobias.pickel@thomann.io>
2025-03-08 11:28:56 +0100

    Updates

---
+++ _next/static/chunks/pages/_app-834fdf131b166999.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{709:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(2933)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(709),n(1159))),_N_E=_.O()}]);
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kochbuch/sw.js', { scope: '/kochbuch/' })})}
