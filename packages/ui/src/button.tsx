edab6d93de660c2974509d8fa098d39eafbce161
tobiaspickel <tobias.pickel@thomann.io>
2025-10-27 14:29:52 +0100

    chore: biome format + lint

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,5 +1,8 @@
-import type {ComponentProps} from "react";
+import type { ComponentProps } from "react";
 
-export const Button = ({type="button",...props}:ComponentProps<'button'>) => {
-    return <button type={type} {...props} />
-}
+export const Button = ({
+	type = "button",
+	...props
+}: ComponentProps<"button">) => {
+	return <button type={type} {...props} />;
+};
